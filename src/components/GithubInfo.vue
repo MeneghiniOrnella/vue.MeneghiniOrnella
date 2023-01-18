6c37c2d419e6a321ccf0db0fe4a98690e394a7d1
Ornella Meneghini <meneghini.ornella@gmail.com>
2023-01-18 19:28:27 -0300

    update githubInfo

--- src/components/GithubInfo.vue
+++ src/components/GithubInfo.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <font-awesome-icon icon="award" /><p>Github Repositories <span>10</span></p>
-        <font-awesome-icon icon="award" /><p>Commits of the year <span>10</span></p>
-        <font-awesome-icon icon="award" /><p>Years of Experience <span>10</span></p>
-        <font-awesome-icon icon="award" /><p>Proyects pinned <span>10</span></p>
-        <img src="https://github-readme-stats.vercel.app/api/?username=MeneghiniOrnella&layout=compact&title_color=a37fa4&text_color=e0d4e1&hide_border=true&langs_count=6&theme=synthwave" width="600" />
+        <p><font-awesome-icon class="font-awesome-icon" icon="award" /> Github Repositories <span>10</span></p>
+        <p><font-awesome-icon class="font-awesome-icon" icon="star" /> Commits of the year <span>10</span></p>
+        <p><font-awesome-icon class="font-awesome-icon" icon="calendar-days" /> Years of Experience <span>10</span></p>
+        <p><font-awesome-icon class="font-awesome-icon" icon="thumbtack" /> Proyects pinned <span>10</span></p>
+        <!-- <img src="https://github-readme-stats.vercel.app/api/?username=MeneghiniOrnella&layout=compact&title_color=a37fa4&text_color=e0d4e1&hide_border=true&langs_count=6&theme=synthwave" width="600" /> -->
     </div>
 </template>
 
@@ -15,5 +15,8 @@ export default {
 </script>
 
 <style>
-
+.font-awesome-icon {
+    color: purple;
+    font-size: 2em;
+}
 </style>
