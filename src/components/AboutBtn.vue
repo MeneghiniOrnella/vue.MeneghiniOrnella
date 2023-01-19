cbaace329d9f5847aacc3c42a82b593e6b52d99a
Ornella Meneghini <meneghini.ornella@gmail.com>
2023-01-19 18:15:56 -0300

    update btns

--- src/components/AboutBtn.vue
+++ src/components/AboutBtn.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="icons-group contact-home">
         <a href="mailto:meneghini.ornella@gmail.com">Contacto</a>
-        <a href="https://www.linkedin.com/in/meneghiniornella/?locale=en_US">LinkedIn English</a>
-        <a href="https://www.linkedin.com/in/meneghiniornella/">LinkedIn Spanish</a>
-        <a href="">Download</a>
+        <a href="https://www.linkedin.com/in/meneghiniornella/?locale=en_US">Linkedin</a>
+        <!-- <a href="@/assets/Meneghini-cv.pdf" download>Download</a> -->
     </div>
 </template>
 
