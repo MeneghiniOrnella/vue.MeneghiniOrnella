512fb6daf93a4aa73343d8bde35b3ab49f336c2f
Ornella Meneghini <meneghini.ornella@gmail.com>
2023-04-13 18:31:35 -0300

    Update Banner

--- src/components/sections/Banner.vue
+++ src/components/sections/Banner.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="banner">
         <h1>Ornella Meneghini</h1>
-        <h2>Full Stack Developer</h2>
+        <h2>Web Developer</h2>
         <BannerBtn />
     </div>
 </template>
